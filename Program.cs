3aae1e81c801a295a71917c4375e9e969855ea4d
Azubuike Okolie <134379140+AzubuikeKevin@users.noreply.github.com>
2025-07-22 21:16:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Prac
+{
+    internal class Program
+    {
+        public delegate int Operation(int a, int b);
+
+        public class Calculator
+        {
+            public static int Addition(int a, int b)
+            {
+                return a + b;
+            }
+
+            public  int Subtraction(int a, int b)
+            {
+                return a - b;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Operation addition = new Operation(Calculator.Addition);
+            int result = addition(20, 5);
+            Console.WriteLine($"Addition result: {result}");
+
+            Operation addition2 = new Operation(calc.Subtraction);
+            int respone = addition2(10,8);
+            Console.WriteLine($"Subtraction result: {respone}");
+        }
+    }
+}
